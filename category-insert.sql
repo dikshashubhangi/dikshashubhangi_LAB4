cbe37080e493c96856b7e29230b7b815a8b604d1
dikshashubhangi <76481935+dikshashubhangi@users.noreply.github.com>
2022-08-23 21:39:40 +0530

    Add files via upload

---
+++ category-insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO category(CAT_ID, CAT_Name)
+VALUES
+	(1,"BOOKS"),
+    (2,"GAMES"),
+    (3,"GROCERIES"),
+    (4,"ELECTRONICS"),
+    (5,"CLOTHES");
---
+++ supplier-insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO supplier(SUPP_ID,SUPP_Name,SUPP_CITY,SUPP_PHONE)
+VALUES
+      (1,"Rajesh Retails","Delhi",'1234567890'),
+      (2,"Appario Ltd.","Mumbai",'2589631470'),
+      (3,"Knome products","Banglore",'9785462315'),
+      (4,"Bansal Retails","Kochi",'8975463285'),
+      (5,"Mittal Ltd.","Lucknow",'7898456532');
